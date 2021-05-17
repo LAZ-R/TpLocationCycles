f4f0d95cffc71e6d98ac7e200c4dd07b1e89d632
LAZ-R <crazydrawer62@gmail.com>
2021-05-17 13:59:54 +0200

    Initial commit

---
+++ src/fr/eni/TpLocationVelo/bo/Cycle.java
@@ -0,0 +1,11 @@
+package fr.eni.TpLocationVelo.bo;
+
+import java.time.LocalDate;
+
+public abstract class Cycle {
+
+    String marque;
+    String modele;
+    LocalDate date_achat;
+
+}
---
+++ src/fr/eni/TpLocationVelo/bo/Gyropode.java
@@ -0,0 +1,4 @@
+package fr.eni.TpLocationVelo.bo;
+
+public class Gyropode extends Cycle{
+}
