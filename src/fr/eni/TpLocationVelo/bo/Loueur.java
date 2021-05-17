b165346c4603572f60815d1f6a33938214ab6e3d
LAZ-R <crazydrawer62@gmail.com>
2021-05-17 15:27:46 +0200

    Javadoc sur la classe Loueur

--- src/fr/eni/TpLocationVelo/bo/Loueur.java
+++ src/fr/eni/TpLocationVelo/bo/Loueur.java
@@ -1,5 +1,10 @@
 package fr.eni.TpLocationVelo.bo;
 
+/** Classe qui représente un Loueur (qui possède des Cycles)
+ *
+ * @author laz_R
+ * @version 1.0
+ */
 public class Loueur {
     // Attributs d'instance
 
@@ -7,6 +12,12 @@ public class Loueur {
     Cycle[] liste_des_cycles = new Cycle[100];
 
     // Méthodes
+
+    /** Méthode qui ajoute des cycles au tableau de Cycles de ce loueur
+     *  (utilisé par les constructeurs).
+     *
+     * @param cycle Cycle
+     */
     public void ajouterCycle(Cycle cycle) {
         for (int i = 0; i < liste_des_cycles.length; i++) {
             if (liste_des_cycles[i] == null) {
@@ -16,6 +27,8 @@ public class Loueur {
         }
     }
 
+    /** Méthode d'affichage des Cycles d'un loueur.
+     */
     public void afficherCycles() {
         System.out.println("Voici les cycles que nous proposons à la location :");
         for (int i = 0; i < liste_des_cycles.length; i++) {
